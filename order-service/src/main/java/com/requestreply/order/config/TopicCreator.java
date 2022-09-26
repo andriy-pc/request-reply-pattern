444e284e58f9a16e5dda7f371c416c575b3d4b74
andriy-pc <48772239+andriy-pc@users.noreply.github.com>
2022-09-26 22:05:37 +0300

    minor improvements (#8)
    removed unused imports; 
    adjusted layout according to google codes-style

--- order-service/src/main/java/com/requestreply/order/config/TopicCreator.java
+++ order-service/src/main/java/com/requestreply/order/config/TopicCreator.java
@@ -12,7 +12,6 @@ import org.springframework.stereotype.Component;
 
 import java.util.Collection;
 import java.util.Set;
-import java.util.concurrent.TimeUnit;
 
 import static java.util.concurrent.TimeUnit.SECONDS;
 import static java.util.stream.Collectors.toList;
@@ -21,25 +20,22 @@ import static java.util.stream.Collectors.toList;
 @RequiredArgsConstructor
 public class TopicCreator {
 
-    private final AdminClient adminClient;
+  private final AdminClient adminClient;
 
-    @Value("#{'${listener.topics.shipment.request}, ${listener.topics.shipment.response}'.split(', ')}")
-    Collection<String> topicNamesToCreate;
+  @Value(
+      "#{'${listener.topics.shipment.request}, ${listener.topics.shipment.response}'.split(', ')}")
+  Collection<String> topicNamesToCreate;
 
-    @EventListener(ApplicationReadyEvent.class)
-    @SneakyThrows
-    public void createTopicsIfNecessary() {
-        ListTopicsResult listTopicsResult = adminClient.listTopics();
-        Set<String> topicNames = listTopicsResult.names().get(10, SECONDS);
-        topicNames.forEach(topicNamesToCreate::remove);
-        adminClient.createTopics(buildTopics(topicNamesToCreate));
-    }
+  @EventListener(ApplicationReadyEvent.class)
+  @SneakyThrows
+  public void createTopicsIfNecessary() {
+    ListTopicsResult listTopicsResult = adminClient.listTopics();
+    Set<String> topicNames = listTopicsResult.names().get(10, SECONDS);
+    topicNames.forEach(topicNamesToCreate::remove);
+    adminClient.createTopics(buildTopics(topicNamesToCreate));
+  }
 
-    Collection<NewTopic> buildTopics(Collection<String> topicsToCreate) {
-        return topicsToCreate
-                .stream()
-                .map(name -> new NewTopic(name, 1, (short) 1))
-                .collect(toList());
-    }
-
+  Collection<NewTopic> buildTopics(Collection<String> topicsToCreate) {
+    return topicsToCreate.stream().map(name -> new NewTopic(name, 1, (short) 1)).collect(toList());
+  }
 }
